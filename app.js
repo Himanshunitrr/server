0745f9d25936b8666ed28461aa35cec55efac1a7
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-09-16 01:22:05 +0530

    setting up things

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const { MONGOURI } = require("./keys")
+const User = require("./models/user")
 
 const app = express()
 const PORT = 5000
